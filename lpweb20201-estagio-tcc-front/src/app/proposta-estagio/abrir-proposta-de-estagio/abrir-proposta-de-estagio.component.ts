ba4526a9614e5b01d4acca21a8ad82e01cd1f050
Vinicius Dias <vdias6849@gmail.com>
2020-06-19 00:04:33 -0300

    feat(frontend): cadastrar avaliação de proposta de estágio.

--- lpweb20201-estagio-tcc-front/src/app/proposta-estagio/abrir-proposta-de-estagio/abrir-proposta-de-estagio.component.ts
+++ lpweb20201-estagio-tcc-front/src/app/proposta-estagio/abrir-proposta-de-estagio/abrir-proposta-de-estagio.component.ts
@@ -4,26 +4,70 @@ import { OrientacaoService } from 'src/app/orientacao.service';
 import { AuthService } from 'src/app/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { delay } from 'rxjs/operators';
+import { AvaliacaoService } from 'src/app/avaliacao.service';
 
 @Component({
   selector: 'app-abrir-proposta-de-estagio',
   templateUrl: './abrir-proposta-de-estagio.component.html',
-  styleUrls: ['./abrir-proposta-de-estagio.component.css']
+  styleUrls: ['./abrir-proposta-de-estagio.component.css'],
 })
 export class AbrirPropostaDeEstagioComponent implements OnInit {
   proposta: any;
+  comentario: string = null;
+  aprovada: boolean;
+  publicada: false;
+  usuario;
+  mostrar: boolean = false;
 
   constructor(
     private propostaEstagio: PropostaEstagioService,
-    private router: ActivatedRoute
-  ) { }
+    private router: ActivatedRoute,
+    private avaliacao$: AvaliacaoService,
+    private auth$: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.router.paramMap.subscribe(
-      params => {
-        this.propostaEstagio.get(params.get('id'))
-          .subscribe(proposta => this.proposta = proposta);
+    this.router.paramMap.subscribe((params) => {
+      this.propostaEstagio.get(params.get('id')).subscribe((proposta) => {
+        this.proposta = proposta;
+        this.verificar();
+      });
+    });
+    this.usuario = this.auth$.usuarioDetalhes();
+  }
+
+  verificar() {
+    this.usuario.groups.find((grupo) => {
+      if (grupo.name == 'Professor') {
+        this.mostrar = true;
       }
-    );
+    });
+  }
+
+  atualizaProposta() {
+    this.router.paramMap.subscribe((params) => {
+      this.propostaEstagio.get(params.get('id')).subscribe((proposta) => {
+        this.proposta = proposta;
+        this.verificar();
+      });
+    });
+  }
+
+  salvarAvaliacao() {
+    this.avaliacao$
+      .avaliar({
+        usuario: this.usuario.id,
+        usuario_id: this.usuario.id,
+        comentario: this.comentario,
+        aprovada: this.aprovada,
+        publicada: this.publicada,
+        proposta: this.proposta.id,
+      })
+      .subscribe((resposta) => {
+        this.publicada = false;
+        this.comentario = null;
+        this.aprovada = null;
+        this.atualizaProposta();
+      });
   }
 }
